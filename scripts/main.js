280d549a8450d175a4c34729e4c8be959d122063
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2020-04-20 22:37:43 +0200

    Initizalize main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,26 @@
 console.log('Działa');
 
+let $average = document.querySelector('.average');
+
+function suma() {
+    let $input = document.querySelectorAll('.poleOceny');
+    console.log($input[0].value);
+}
+
+
+// ciało programu, w nie będa wywoływane poszczególne funkcje
+function setup() {
+    console.log('Funkcja setup');
+    suma();
+}
+
+
+// po kliknięciu w przycisk startuje funkcja setup
+$average.addEventListener('click', setup);
+
+
+// referencja do li
+
 /* 
 
  if (value >=1 || value <=6)  {
